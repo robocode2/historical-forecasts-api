476336b40b8a69e930100b239c982a03631728ee
robocode2 <rabie.abbas@code.berlin>
2024-12-01 17:19:43 +0100

    db and test db configuration file instructions

--- API-LB4/src/datasources/scrapy-app.db.ts
+++ API-LB4/src/datasources/scrapy-app.db.ts
@@ -1,36 +1,40 @@
 import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
 import {juggler} from '@loopback/repository';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const caCert = fs.readFileSync(path.resolve(__dirname, 'ca-certificate.crt')).toString();
 
 const mainConfig = {
   name: 'defaultdb',
   connector: 'postgresql',
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  url: '',
+  url: '', // Add your Database URL without ssl mode parameter
   user: process.env.DB_USER,
   password: process.env.DB_PASSWD,
-  database: process.env.DB_DEV_SCHEMA
+  database: process.env.DB_DEV_SCHEMA,
+  ssl: {
+    ca: caCert,
+    rejectUnauthorized: true,
+  },
 };
 
 const testConfig = {
   name: process.env.TEST_DB_NAME,
-  connector: process.env.DB_CONNECTOR,
-  host: process.env.DB_HOST,
-  database: process.env.DB_TEST,
-  port: process.env.DB_PORT,
-  url: process.env.DB_TEST_URL,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWD,
+  connector: "postgresql",
+  host: '127.0.0.1', 
+  port: 5434,          // Match the port from your docker-compose.yml
+  user: '',    // Add your Postgres user
+  password: '', // Add your Postgres password
+  database: '', // Add the name of your database
+  ssl: false,       
 };
 
-// this is the important part
 const config = process.env.NODE_ENV === 'test' ? testConfig : mainConfig;
 
 
-// Observe application's life cycle to disconnect the datasource when
-// application is stopped. This allows the application to be shut down
-// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
-// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+
 @lifeCycleObserver('datasource')
 export class ScrapyAppDb extends juggler.DataSource implements LifeCycleObserver {
   static dataSourceName = 'ScrapyApp';
